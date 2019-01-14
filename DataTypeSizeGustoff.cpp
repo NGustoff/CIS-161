957d18560aa6f6aaf3c580f160b65e82c61cf893
NGustoff <46538853+NGustoff@users.noreply.github.com>
2019-01-14 11:55:41 -0600

    Add files via upload

---
+++ DataTypeSizeGustoff.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "The size of a Character " << sizeof(char) << endl;
+	cout << "The size of an Interger " << sizeof(int) << endl;
+	cout << "The size of a Float " << sizeof(float) << endl;
+	cout << "The size of a Double " << sizeof(double) << endl;
+
+	system("pause");
+	return 0;
+
+}
